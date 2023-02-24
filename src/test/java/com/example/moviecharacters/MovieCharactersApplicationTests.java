855c52ae5154a1aca154007f7e791afc6bd0ffb1
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-02-24 11:47:35 +0100

    first commit

---
+++ src/test/java/com/example/moviecharacters/MovieCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviecharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharactersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
