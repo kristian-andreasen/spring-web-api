529116eb8e1d8f6b27c2a7675c817069d21c77ef
Frederik Darling <frederikdarling@live.dk>
2023-02-28 12:28:01 +0100

    more data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,48 +12,67 @@ VALUES (DEFAULT,'Harry Potter is a series of seven fantasy novels written by Bri
 INSERT INTO tb_movie
 VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy', 'https://www.vintagemovieposters.co.uk/wp-content/uploads/2020/11/IMG_0689-scaled.jpeg','Star Wars: Episode IV - A New Hope',1977,'https://www.youtube.com/watch?v=s0oZPZCBk6E');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Irwin Kersner', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode V - The Empire Strikes Back',1980,'');
+VALUES (DEFAULT, 'Irvin Kershner', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode V - The Empire Strikes Back',1980,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Richard Mcraund', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode VI - Return of the Jedi',1983,'');
+VALUES (DEFAULT, 'Richard Marquand', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode VI - Return of the Jedi',1983,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode I - The Phantom Menace',1999,'');
+VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy','' ,'Star Wars: Episode I - The Phantom Menace',1999,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy', '','Star Wars: Episode II - Attack of the Clones',2002,'');
+VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy', '','Star Wars: Episode II - Attack of the Clones',2002,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy', '','Star Wars: Episode III - Revenge of the Sith',2005,'');
+VALUES (DEFAULT, 'George Lucas', 'Star Wars', 'Action, Adventure, Fantasy', '','Star Wars: Episode III - Revenge of the Sith',2005,'https://www.youtube.com/example');
 
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Steven Spielberg', 'Indian Jones', 'Action, Adventure','' ,'Indiana Jones and the Raiders of the Lost Ark',1981,'');
+VALUES (DEFAULT, 'Steven Spielberg', 'Indiana Jones', 'Action, Adventure','' ,'Indiana Jones and the Raiders of the Lost Ark',1981,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Steven Spielberg', 'Indian Jones', 'Action, Adventure','' ,'Indiana Jones and the Temple of Doom',1984,'');
+VALUES (DEFAULT, 'Steven Spielberg', 'Indiana Jones', 'Action, Adventure','' ,'Indiana Jones and the Temple of Doom',1984,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Steven Spielberg', 'Indian Jones', 'Action, Adventure', '','Indiana Jones and the Last Crusade',1989,'');
+VALUES (DEFAULT, 'Steven Spielberg', 'Indiana Jones', 'Action, Adventure', '','Indiana Jones and the Last Crusade',1989,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Steven Spielberg', 'Indian Jones', 'Action, Adventure','' ,'Indiana Jones and the Kingdom of the Krystal Skull',2008,'');
+VALUES (DEFAULT, 'Steven Spielberg', 'Indiana Jones', 'Action, Adventure','' ,'Indiana Jones and the Kingdom of the Krystal Skull',2008,'https://www.youtube.com/example');
 
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Chris Columbus', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Sorcerers Stone',2001,'');
+VALUES (DEFAULT, 'Chris Columbus', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Sorcerers Stone',2001,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Chris Columbus', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Chamber of Secrets',2002,'');
+VALUES (DEFAULT, 'Chris Columbus', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Chamber of Secrets',2002,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Alfonzo cuaron', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Prisoner of Azkaban',2004,'');
+VALUES (DEFAULT, 'Alfonso Cuarón', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Prisoner of Azkaban',2004,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'Mike Newell', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Goblet of Fire',2005,'');
+VALUES (DEFAULT, 'Mike Newell', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Goblet of Fire',2005,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Order of Phoenix',2007,'');
+VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Order of Phoenix',2007,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Half-Blood Prince',2009,'');
+VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy', '','Harry Potter and the Half-Blood Prince',2009,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Deathly Hallows part 1',2010,'');
+VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Deathly Hallows part 1',2010,'https://www.youtube.com/example');
 INSERT INTO tb_movie
-VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Deathly hallows part 2',2011,'');
+VALUES (DEFAULT, 'David Yates', 'Harry Potter', 'Adventure, Family, Fantasy','' ,'Harry Potter and the Deathly hallows part 2',2011,'https://www.youtube.com/example');
 
-
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Luke Skywalker');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Han Solo');
+INSERT INTO character
+VALUES(DEFAULT, 'Princess Leia', 'female', NULL, 'Leia Organa');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Jar Jar Binks');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'other', NULL, 'IG-88');
 INSERT INTO character
 VALUES(DEFAULT, 'Anakin Skywalker', 'male', NULL, 'Darth Vader');
 
 INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Harry Potter');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Ron Weasley');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'female', NULL, 'Hermione Granger');
+INSERT INTO character
 VALUES(DEFAULT, NULL, 'male', NULL, 'Dobby');
 
 INSERT INTO character
-VALUES(DEFAULT, NULL, 'female', NULL, 'Short Round');
+VALUES(DEFAULT, 'Dr.Jones', 'male', NULL, 'Indiana Jones');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'female', NULL, 'Marion');
+INSERT INTO character
+VALUES(DEFAULT, NULL, 'male', NULL, 'Short Round');
