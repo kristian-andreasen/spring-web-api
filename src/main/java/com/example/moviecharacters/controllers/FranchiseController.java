18111b09d3aa8b5aaefc23ed750bb586e58fbdf1
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-03-02 14:29:32 +0100

    movieController is working. You can make a HTTP GET request to get a movie

--- src/main/java/com/example/moviecharacters/controllers/FranchiseController.java
+++ src/main/java/com/example/moviecharacters/controllers/FranchiseController.java
@@ -1,35 +1,35 @@
 package com.example.moviecharacters.controllers;
+import com.example.moviecharacters.dto.character.FranchiseGetDTO;
+import com.example.moviecharacters.mappers.FranchiseMapper;
 import com.example.moviecharacters.models.Franchise;
 import com.example.moviecharacters.repositories.FranchiseRepository;
 import com.example.moviecharacters.services.FranchiseServiceImpl;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/franchises") //base path
 @RestController
 public class FranchiseController {
     private final FranchiseServiceImpl franchiseService;
-    private final FranchiseRepository franchiseRepository;
+    private final FranchiseMapper franchiseMapper;
 
-    public FranchiseController(FranchiseServiceImpl franchiseService, FranchiseRepository franchiseRepository) {
-        this.franchiseRepository = franchiseRepository;
+    public FranchiseController(FranchiseServiceImpl franchiseService,FranchiseMapper franchiseMapper) {
+        this.franchiseMapper = franchiseMapper;
         this.franchiseService = franchiseService;
     }
 
-    @GetMapping()
-    public Iterable<Franchise> getAllFranchises() {
-        return franchiseService.findAll();
-    }
+    /*@GetMapping()
+    public ResponseEntity<FranchiseGetDTO> getAllFranchises() {
+        FranchiseGetDTO franchiseGetDTO = franchiseMapper.toFranchiseDto(franchiseService.findAll());
+        return ResponseEntity.ok(franchiseGetDTO);
+    }*/
+
 
     @GetMapping("/{id}")
-    public Franchise getFranchiseById(@PathVariable int id) {
-        return franchiseService.findById(id);
-
-        /*if (franchise.isPresent()) {
-            return franchise.get();
-        } else {
-            // throw a ResponseStatusException with a NOT_FOUND status code and a message.
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Franchise not found");
-        }*/
+    public ResponseEntity<FranchiseGetDTO> getFranchiseById(@PathVariable int id) {
+        FranchiseGetDTO franchiseGetDTO = franchiseMapper.toFranchiseDto(franchiseService.findById(id));
+        return ResponseEntity.ok(franchiseGetDTO);
     }
 
     @PostMapping()
@@ -37,7 +37,7 @@ public class FranchiseController {
         return franchiseService.add(franchise);
     }
 
-    @PutMapping("/{id}")
+    /*@PutMapping("/{id}")
     public Franchise updateFranchise(@PathVariable int id, @RequestBody Franchise updatedFranchise) {
         Franchise existingFranchise = franchiseService.findById(id);
 
@@ -49,7 +49,7 @@ public class FranchiseController {
 
         //not sure about this though...
         return franchiseRepository.save(existingFranchise);
-    }
+    }*/
 
     @DeleteMapping("/{id}")
     public void deleteFranchise(@PathVariable int id) {
--- src/main/java/com/example/moviecharacters/mappers/FranchiseMapper.java
+++ src/main/java/com/example/moviecharacters/mappers/FranchiseMapper.java
@@ -2,9 +2,11 @@ package com.example.moviecharacters.mappers;
 import com.example.moviecharacters.dto.character.FranchiseGetDTO;
 import com.example.moviecharacters.models.Franchise;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FranchiseMapper {
+    @Mapping(target = "movies", source = "movies")
     FranchiseGetDTO toFranchiseDto(Franchise franchise);
     Franchise toFranchise(FranchiseGetDTO dto);
 }
